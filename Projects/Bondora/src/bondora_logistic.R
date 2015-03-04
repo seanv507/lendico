e1ef4671d96c3cc3df8af93827d0f5041949ef58
sean violante <sean.violante@gmail.com>
2015-03-04 17:49:28 +0100

    split off functions

--- Projects/Bondora/src/bondora_logistic.R
+++ Projects/Bondora/src/bondora_logistic.R
@@ -1,85 +1,10 @@
-require(xlsx)
-require(plyr)
-require(ggplot2)
-require(GGally)
-require(gridExtra)
-require(data.table)
-require(lubridate)
-require(glmnet)
-require(ROCR)
-require(reshape2)
 
-# load & format
-# select
-# train test
-# model list
-# train ->test score
-# plot results
-# store models and results 
-# store models with training data
-# split out model from generic code...
-
-
-summary_reals<-function(dt, target,real_vars){
-  list_sum<-lapply(real_vars,function (f) dt[,c(variable=f,N=.N, as.list(summary(get(f)))),keyby=get(target)])
-  df_sum<-do.call(rbind,list_sum)
-  setnames(df_sum,'get',target)
-}
-
-summary_factors<-function(dt, target,factor_vars){
-  # calc N, mean, std err
-  
-  list_sum<-lapply(factor_vars,function (f) dt[,.(fact=f, N=.N, rate=sum(get(target)==1)/.N, std_err=sum(get(target)==1)*(.N-sum(get(target)==1))/.N^3),keyby=f])
-  
-  # create combined dataframe by adding column with variable name
-  for (i in 1:length(factor_vars))   setnames(list_sum[[i]], factor_vars[i], 'value')
-  df_sum<-do.call(rbind,list_sum)
-}
-
-glmdf<-function(cv.fit){
-  # create data frame of of metric and coefficients
-  metric_df<-data.frame(lambda=cv.fit$lambda, type=cv.fit$name, cvm=cv.fit$cvm, cvup=cv.fit$cvup,cvlo=cv.fit$cvlo,cvsd=cv.fit$cvsd,nzero=cv.fit$nzero)
-  coef_df<-as.data.frame(as.matrix(coef(cv.fit$glmnet.fit)))
-  coef_df$coef=rownames(coef_df)
-  colnames(coef_df)<-cv.fit$lambda
-  coef_df<-melt(coef_df,variable.name='lambda')
-  list(metrics=metric_df,coefs=coef_df, lambda_crit=list(name=cv.fit$name,min=cv.fit$lambda.min, se1=cv.fit$lambda.1se))
-}
-
-glm_boot_gen<-function(family, measure, lambda, pred_type){
-
-  function(data,frequencies){
-    y<-data[,1]
-    x<-data[,2:ncol(data)]
-    weights<-frequencies
-    cv.fit<-cv.glmnet(x, y, weights,family=family, type.measure=measure)
-    c<-coef(cv.fit,lambda)
-    v<-predict(cv.fit,x,s=lambda,type=pred_type)
-    v
-  }
-  
-}
-
-gini<-function(predictions,labels){
-  pred<-prediction(predictions,labels)
-  perf<-performance(pred, measure = "auc")
-  perf@'y.values'[[1]]*2-1
-  
-}
-
-logit<-function (x) 1/(1+exp(-x))
-
-logloss<-function(actual,target){
-  err=-((target==1)*log(actual)+(target==0)*log(1-actual))
-  mean(err)
-}
-
-dict<-read.xlsx('C:\\Users\\Sean Violante\\Documents\\Projects\\lendico\\Projects\\Bondora\\data\\BondoraAll03.xlsm',
+dict<-read.xlsx('..\\data\\BondoraAll03.xlsm',
                 sheetName='BondoraLoanDictionaryClean', stringsAsFactors=FALSE)
 
 load_data<-function(dict){
   dates_names<-unique(dict$column[dict$type=='date'])
-  loandata<-read.csv('C:\\Users\\Sean Violante\\Documents\\Projects\\lendico\\Projects\\Bondora\\data\\LoanData_20150204_es.csv', 
+  loandata<-read.csv('..\\data\\LoanData_20150204_es.csv', 
                      stringsAsFactors=FALSE,fileEncoding="UTF-8")
   
   loandata[dates_names]<-lapply(loandata[dates_names], function (x) as.Date(x,format='%Y-%m-%d'))
@@ -201,43 +126,66 @@ loandata$defaulted_before_6m[loandata$selected_loans_6m]<-!is.na(loandata$Defaul
 loandata_dt<-data.table(loandata)
 
 
-loans<-loandata[loandata$selected_loans_6m_train==1,]
-loans_dt<-loandata_dt[loandata_dt$selected_loans_6m_train==1,]
+loans_tr<-loandata[loandata$selected_loans_6m_train==1,]
+loans_te<-loandata[loandata$selected_loans_6m_train==0,]
+loans_dt_tr<-loandata_dt[loandata_dt$selected_loans_6m_train==1,]
+loans_dt_te<-loandata_dt[loandata_dt$selected_loans_6m_train==0,]
 
 
 target_variable<-'defaulted_before_6m'
 
 
-rate_overall<-loans_dt[,c(N=.N,rate=mean(get(target_variable)))]
+rate_overall<-loans_dt_tr[,c(N=.N,rate=mean(get(target_variable)))]
 
-loans_dt_ints_all<-summary_factors(loans_dt, target_variable,request_ints)
-loans_dt_facs_all<-summary_factors(loans_dt, target_variable,request_factors)
-loans_dt_reals_all<-summary_reals(loans_dt, target_variable,request_reals)
+loans_dt_ints_all<-summary_factors(loans_dt_tr, target_variable,request_ints)
+loans_dt_facs_all<-summary_factors(loans_dt_tr, target_variable,request_factors)
+loans_dt_reals_all<-summary_reals(loans_dt_tr, target_variable,request_reals)
+loans_dt_ints_all$data="train"
+loans_dt_facs_all$data="train"
+loans_dt_reals_all$data="train"
+
+loans_dt_te_ints_all<-summary_factors(loans_dt_te, target_variable,request_ints)
+loans_dt_te_facs_all<-summary_factors(loans_dt_te, target_variable,request_factors)
+loans_dt_te_reals_all<-summary_reals(loans_dt_te, target_variable,request_reals)
+loans_dt_te_ints_all$data="test"
+loans_dt_te_facs_all$data="test"
+loans_dt_te_reals_all$data="test"
+
+loans_dt_eqi_ints_all<-summary_factors_p(loans_dt_tr, "eqi_pd_6m",request_ints)
+loans_dt_eqi_facs_all<-summary_factors_p(loans_dt_tr, "eqi_pd_6m",request_factors)
+loans_dt_eqi_reals_all<-summary_reals(loans_dt_tr,"eqi_pd_6m",request_reals)
+loans_dt_eqi_ints_all$data="equi"
+loans_dt_eqi_facs_all$data="equi"
+loans_dt_eqi_reals_all$data="equi"
 
 report_name<-paste0('univariate_factors_no_resched_',target_variable,'.pdf')
-
-p<-ggplot(data=loans_dt_facs_all, aes(x=value, y=rate*100,size=N, ymin=rate*100-100*std_err,ymax=rate*100+100*std_err) ) +  
+mg<-rbind(loans_dt_facs_all,loans_dt_te_facs_all, loans_dt_eqi_facs_all)
+p<-ggplot(data=mg, aes(color=data,x=value, y=rate*100,size=N, ymin=rate*100-100*std_err,ymax=rate*100+100*std_err) ) +  
   geom_point()+scale_size_area()+geom_hline(yintercept=rate_overall[['rate']]*100) + coord_flip()
-plots<-dlply(loans_dt_facs_all,"fact",  function(x) `%+%`(p,x)+xlab(x$fact[[1]])  )
+plots<-dlply(mg,"fact",  function(x) `%+%`(p,x)+xlab(x$fact[[1]])  )
 ml = do.call(marrangeGrob, c(plots, list(nrow=4, ncol=2)))
 
 ggsave(report_name, ml, width=21,height=27)
 
 report_name<-paste0('univariate_ints_no_resched_',target_variable,'.pdf')
-p<-ggplot(data=loans_dt_ints_all, aes(x=value, y=rate*100,size=N, ymin=rate*100-100*std_err,ymax=rate*100+100*std_err) ) +  
+mg<-rbind(loans_dt_ints_all,loans_dt_te_ints_all, loans_dt_eqi_ints_all)
+p<-ggplot(data=mg, aes(color=data, x=value, y=rate*100,size=N, ymin=rate*100-100*std_err,ymax=rate*100+100*std_err) ) +  
   geom_point()+scale_size_area()+geom_hline(yintercept=rate_overall[['rate']]*100) + coord_flip()
 # replace current dataframe `%+%`, and provide labels
-plots<-dlply(loans_dt_ints_all,"fact",  function(x) `%+%`(p,x)+xlab(x$fact[[1]])  )
+plots<-dlply(mg,"fact",  function(x) `%+%`(p,x)+xlab(x$fact[[1]])  )
 ml = do.call(marrangeGrob, c(plots, list(nrow=4, ncol=2)))
 ggsave(report_name, ml, width=21,height=27)
 
 
 report_name<-paste0('univariate_reals_no_resched_',target_variable,'.pdf')
-loans_dt_reals_all$default<-loans_dt_reals_all$defaulted_before_6m==TRUE
-# need dataframe just because of invalid column names
+
+# mg<-rbind(loans_dt_reals_all,loans_dt_te_reals_all)
+# loans_dt_eqi_reals_all
+# loans_dt_reals_all$default<-loans_dt_reals_all$defaulted_before_6m==TRUE
+# # need dataframe just because of invalid column names
 ml<-ggplot(data=data.frame(loans_dt_reals_all),
-           aes_string(x=target_variable,ymin='Min.',lower='X1st.Qu.', middle='Median', upper='X3rd.Qu.', ymax='Max.')) + 
-            geom_boxplot(stat='identity') +facet_wrap(~variable,scales='free')
+            aes_string(x=target_variable,ymin='Min.',lower='X1st.Qu.', middle='Median', upper='X3rd.Qu.', ymax='Max.')) + 
+             geom_boxplot(stat='identity') +facet_wrap(~variable,scales='free')
 ggsave(report_name, ml, width=21,height=27)
 
 
@@ -251,13 +199,6 @@ report_name<-paste0('hist_reals_no_resched_',target_variable,'.pdf')
 p<-ggplot(data=loans,aes(y=..density..,fill=defaulted_before_6m))+geom_histogram(alpha=0.4,position='identity')
 
 plots<-llply(request_reals,  function(x) p+aes_string(x)  )
-ml = do.call(marrangeGrob, c(plots, list(nrow=4, ncol=2)))
-ggsave(report_name, ml, width=21,height=27)
-
-
-p<-ggplot(data=loans_dt_facs_all, aes(x=value, y=rate*100,size=N, ymin=rate*100-100*std_err,ymax=rate*100+100*std_err) ) +  
-  geom_point()+scale_size_area()+geom_hline(yintercept=rate_overall[['rate']]*100) + coord_flip()
-plots<-dlply(loans_dt_facs_all,"fact",  function(x) `%+%`(p,x)+xlab(x$fact[[1]])  )
 ml = do.call(marrangeGrob, c(plots, list(nrow=4, ncol=2)))
 ggsave(report_name, ml, width=21,height=27)
 
@@ -276,15 +217,14 @@ predict_cols<-c('NewPaymentToIncome','LiabilitiesToIncome','UseOfLoan',
                 'education_id','marital_status_id','employment_status_id','Employment_Duration_Current_Employer','occupation_area',
                 'home_ownership_type_id')
 
-
-
-loans_selected<-loandata[loandata$selected_loans_6m,]
-
 pai=c('NewPaymentToIncome','LiabilitiesToIncome','UseOfLoan',
-'VerificationType','credit_score','CreditGroup')
+      'VerificationType','credit_score','CreditGroup')
 ggpairs(data=loans[,pai])
 
-y1<-as.matrix(loans_selected$defaulted_before_6m==TRUE)
+
+
+y1_tr<-as.matrix(loans_tr$defaulted_before_6m==TRUE)
+y1_te<-as.matrix(loans_te$defaulted_before_6m==TRUE)
 
 #x1<-model.matrix(AD~(NewPaymentToIncome+LiabilitiesToIncome)*(VerificationType + Gender+ UseOfLoan+education_id+marital_status_id+employment_status_id+Employment_Duration_Current_Employer+occupation_area+home_ownership_type_id)-1,data=loandata[selected_loans,])
 #y<-loandata[selected_loans,'AD']==1
@@ -299,29 +239,42 @@ base_formula_no_credits<-formula(defaulted_before_6m~VerificationType+Gender+Use
                         employment_status_id+Employment_Duration_Current_Employer+work_experience_10+occupation_area+
                         marital_status_id+nr_of_dependants_1+home_ownership_type_id  -1)
 
-# factors_formula<-update.formula(reals_formula, . ~VerificationType+Gender+credit_score+CreditGroup+UseOfLoan+education_id+
-#                                   marital_status_id+nr_of_dependants+
-#                                   employment_status_id+Employment_Duration_Current_Employer+work_experience+occupation_area+
-#                                   home_ownership_type_id+BondoraCreditHistory+Rating_V0+Rating_V1-1)
-x1<-model.matrix(factors_formula,data=loans_selected)
-x1<-model.matrix(defaulted_before_6m~VerificationType+AppliedAmountToIncome-1,data=loans_selected)
 
-x1<-model.matrix(base_formula_no_credits,data=loans_selected)
+equi_formula<- formula(defaulted_before_6m~
+                        equi_age_fac + 
+                        equi_marital_fac +
+                        equi_education_fac +
+                        equi_employment_status_fac + 
+                        equi_employment_length_fac +
+                        equi_net_income_fac +
+                        equi_principal_duration_fac +
+                        equi_loan_purpose_fac -1)
+            
+amount_formula<-formula(defaulted_before_6m~VerificationType -1)
 
-cv.fit<-cv.glmnet(x1,y1,family='binomial', type.measure='auc')
-predict_tr<-predict(cv.fit,x1,type='response',s='lambda.1se')
-coef(cv.fit,s='lambda.1se')
-gini(predict_tr,y1)
+model_formula<-amount_formula
+#x1<-model.matrix(model_formula,data=loans)
+x1_tr<-model.matrix(model_formula,data=loans_tr)
+x1_te<-model.matrix(model_formula,data=loans_te)
+
+cv.fit<-cv.glmnet(x1_tr,y1_tr,family='binomial') #,type.measure='auc')
+s_max<-cv.fit$lambda[length(cv.fit$lambda)]
+
+s='lambda.1se'
+s='lambda.min'
+s=s_max
+predict_tr<-predict(cv.fit,x1_tr,type='response',s=s)
+predict_te<-predict(cv.fit,x1_te,type='response',s=s)
+coef(cv.fit,s=s)
+gini(predict_tr,y1_tr)
+gini(predict_te,y1_te)
+
+plot(cv.fit)
 
 ggplot(as.data.frame(predict_tr),aes(x=`1`))+geom_bar()
 
 
 
-predict_tr<-predict(fit,x1,type='response',s='s1')
-coef(fit)
-gini(predict_tr,y1)
-
-summary(loandata[(request_fields)])
 
 #extract coefs
 coef(cv.fit,s='lambda.1se')
