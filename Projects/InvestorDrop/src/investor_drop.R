36a1c4eedd05cf94e8fc563633cceaf3ba224555
sean violante <sean.violante@gmail.com>
2015-05-11 10:28:31 +0200

    unused

--- Projects/InvestorDrop/src/investor_drop.R
+++ Projects/InvestorDrop/src/investor_drop.R
@@ -1,12 +1,15 @@
-source('../../../lib/read_postgresql.R')
 
+setwd("~/Projects/lendico/Projects/InvestorDrop/src")
+source('../../../lib/read_postgresql.R')
+require(data.table)
+require(ggplot2)
+require(zoo)
 con_drv=get_con()
 
 #sql='select * from base.loan_request limit 10'
-
-sql_loan_requests="select * from  il.global_borrower_accounts_cohort"
-#sql_loan_requests="select * from  il.bid_transactions"
-
-
+sql_loan_requests="select * from  il.global_borrower_loan_requests_cohort"
 res<-dbGetQuery(con_drv[[1]],sql_loan_requests)
+
+loan_requests<-data.table(res)
+
 #resTables<-dbListTables(con)
