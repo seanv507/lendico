4a8902e7037debf34305c5e40c7147a4077bf261
sean violante <sean.violante@gmail.com>
2015-05-11 10:24:11 +0200

    using sqlalchemy

--- lib/dwh.py
+++ lib/dwh.py
@@ -4,11 +4,13 @@ Created on Wed Jan 07 15:36:39 2015
 
 @author: Sean Violante
 """
+from sqlalchemy import create_engine
 
-import psycopg2 as pg
+#import psycopg2 as pg
 
 def get_DWH():
-    """ Connect to DWH.
-    """
-    conn=pg.connect("host='10.11.0.1' dbname='lendico' user='sviolante' password='3qcqHngX'")
-    return conn
+	""" Connect to DWH.
+	"""
+	#conn=pg.connect("host='10.11.0.1' dbname='lendico' user='sviolante' password='3qcqHngX'")
+	engine = create_engine('postgresql://sviolante:3qcqHngX@10.11.0.1:5432/lendico')
+	return engine
