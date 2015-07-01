8623b6d888a2bd0b3d2d192b0b25588751053782
sean violante <sean.violante@gmail.com>
2015-07-01 11:25:25 +0200

    updated fields etc

--- Projects/Returns/src/score_germany.R
+++ Projects/Returns/src/score_germany.R
@@ -1,11 +1,16 @@
-
+require('lubridate')
 source('../../../lib/read_postgresql.R')
 
 con_drv=get_con()
 lendi_loans<-get_loans(con_drv)
+
 lendi_loans$duration_months<-lendi_loans$duration/30
 facs<-c("category")
 lendi_loans[facs]<-lapply(lendi_loans[facs],as.factor)
+lendi_loans['in_arrears_since_combined_days']=NA
+lendi_loans[!is.na(lendi_loans['in_arrears_since_combined']),'in_arrears_since_combined_days'] = 
+  interval(lendi_loans[!is.na(lendi_loans['in_arrears_since_combined']),'in_arrears_since_combined'],now())/edays(1)
+lendi_loans['in_arrears_since_combined_days']=interval(lendi_loans['in_arrears_since_combined'],now())/edays(1)
 #   loans_attribute_late<-merge(x = loans_attribute, y = loans_first_lates, by.x="id_loan", by.y = "fk_loan", all=FALSE)
 #   loans_first_lates<-get_first_lates(con_drv)
 #   
@@ -48,18 +53,21 @@ first_lates<-get_first_lates(con_drv)
 loans_account_attribute_lates<-merge(x=loans_attribute,y=first_lates,by.x="id_loan",by.y="fk_loan")
 loans_account_attribute_lates$total_net_income<-total_net_income_it(loans_account_attribute_lates)
 
-loans_attribute_late$gender_f<-as.factor(loans_attribute_late$gender)
+loans_account_attribute_lates$gender_f<-as.factor(loans_account_attribute_lates$gender)
+
+a<-loans_account_attribute_lates[!is.na(loans_account_attribute_lates['in_arrears_since_combined_days'])&loans_account_attribute_lates['in_arrears_since_combined_days']>=91,]
+write.csv(a,file('clipboard-128'))
 
 
 
 
-
-my.fit<-survfit(Surv(loans_attribute_late$surv_time_14,loans_attribute_late$surv_14)~1)
-my.fit.gender<-survfit(Surv(loans_attribute_late$surv_time_14,loans_attribute_late$surv_14)~loans_attribute_late$gender_f)
+my.fit<-survfit(Surv(loans_account_attribute_lates$surv_time_14,loans_account_attribute_lates$surv_14)~1)
+my.fit.gender<-survfit(Surv(loans_account_attribute_lates$surv_time_14, loans_account_attribute_lates$surv_14)
+                       ~loans_account_attribute_lates$gender_f)
 
 plot(my.fit, main="Kaplan-Meier estimate >14days", xlab="time", ylab="survival function")
-lines(my.fit1, main="Kaplan-Meier estimate", xlab="time", ylab="survival function", col=seq(2,3))
-z1<-levels(factor(loans_attribute_late$gender))
+lines(my.fit.gender, main="Kaplan-Meier estimate", xlab="time", ylab="survival function", col=seq(2,3))
+z1<-levels(factor(loans_account_attribute_lates$gender))
 z2<-c("overall",z1)
 legend(300,.2,legend=z2,fill=seq(3))
 # change to 400? use ymin =.6
@@ -70,25 +78,28 @@ legend(300,.2,legend=z2,fill=seq(3))
 
 
 
-vars<-["marital_status","user_age", "rating","user_expenses_home","user_income_employment_status","user_income_employment_type"]
+vars<-c("marital_status","user_age", "rating","user_expenses_home","user_income_employment_status","user_income_employment_type")
 
 kmplot<-function(f){
-  f_string<-paste0("Surv(loans_attribute_late$surv_time_14,loans_attribute_late$surv_14)~loans_attribute_late$",paste0(f,"_f"))
+  f_string<-paste0("Surv(loans_account_attribute_lates$surv_time_14,loans_account_attribute_lates$surv_14)~loans_account_attribute_lates$",paste0(f,"_f"))
   z<-survfit(as.formula(f_string))
   
   a<-plot(my.fit, main=paste("Kaplan-Meier estimate >14 days",f), xlab="time", ylab="survival function",ymin=0.6)
-  z1<-levels(loans_attribute_late[[paste0(f,"_f")]])
+  z1<-levels(loans_account_attribute_lates[[paste0(f,"_f")]])
   z2<-c("overall",z1)
   lines(z,  col=seq(2,length(z2)))
   legend(300,1,legend=z2,fill=seq(length(z2)))
   z
 }
 
-my.fit.user_expenses_home<-survfit(Surv(loans_attribute_late$surv_time_14,loans_attribute_late$surv_14)
-                                   ~loans_attribute_late$user_expenses_home_f)
+kmplot(vars[1])
+
+f1 =vars[1]
+my.fit.user_expenses_home<-survfit(Surv(loans_account_attribute_lates$surv_time_14,loans_account_attribute_lates$surv_14)
+                                   ~loans_account_attribute_lates$user_expenses_home_f)
 
 plot(my.fit, main=paste("Kaplan-Meier estimate >14 days","user_expenses_home"), xlab="time", ylab="survival function",ymin=0.6)
-z1<-levels(loans_attribute_late$user_expenses_home_f)
+z1<-levels(loans_account_attribute_lates$user_expenses_home_f)
 z2<-c("overall",z1)
 
 lines(my.fit.user_expenses_home,  col=seq(2,length(z2)))
